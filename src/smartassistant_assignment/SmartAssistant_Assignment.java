3ebdf48a24d7e031bb6b7fbef31897c245348bfb
Hiroki <mitsuke1116@gmail.com>
2015-12-06 21:52:46 +1000

    Completed

--- src/smartassistant_assignment/SmartAssistant_Assignment.java
+++ src/smartassistant_assignment/SmartAssistant_Assignment.java
@@ -5,11 +5,14 @@
  */
 package smartassistant_assignment;
 
+import com.opencsv.CSVWriter;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -19,67 +22,77 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * The whole process can be described in the following way. First of all, the
+ * input data is going to be extracted and put into a matrix, which is made
+ * ArrayList<String>. Then, the required transformation will be made on the each
+ * product inside of the matrix. Once a product has the transformation, the
+ * product will be stored in an array of String. Finally, the array, which will
+ * contain all the products with required transformation, will be written into
+ * the CSV file.
  *
- * @author takaha
+ * @author Hiroki Takahashi
  */
 public class SmartAssistant_Assignment {
+
+    //The following two variables have to be modified, based on the directories of the input and output files.
+    static String inputFile = "C:\\Users\\takaha\\Desktop\\SmartAssistant\\products.csv";
+    static String outputFile = "C:\\Users\\takaha\\Desktop\\SmartAssistant\\result.csv";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-        String inputCSV = "C:\\Users\\takaha\\Desktop\\SmartAssistant\\products.csv";
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(inputCSV), "UTF8"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), "UTF8"));
         String line;
         String cvsSplitBy = ";";
 
-        ArrayList<ArrayList<String>> content = new ArrayList<>();
+        //This matrix will contain all the input data from products.csv
+        ArrayList<ArrayList<String>> inputMatrix = new ArrayList<>();
 
-        //Create a matrix, which contains all the date from products.csv
+        //Fill in the matrix of the input data
         while ((line = br.readLine()) != null) {
             String[] splittedLine = line.split(cvsSplitBy);
-
             ArrayList<String> temp = new ArrayList<>();
+
             for (int i = 0; i < splittedLine.length; i++) {
+                //The quote character is removed here, so that the transoformation would be easy to make later
                 temp.add(splittedLine[i].replaceAll("\"", ""));
             }
-            content.add(temp);
+            inputMatrix.add(temp);
         }
 
-        /*
-        //See the content of the matrix of products.csv
-        for (int i = 0; i < content.size(); i++) {
-            ArrayList<String> temp = content.get(i);
-            for (int l = 0; l < temp.size(); l++) {
-                System.out.println("Row: " + i + " Col: " + l + " : " + temp.get(l));
-            }
-        }*/
+        //All the data have been obtained, so the next step is to make the transformation on them
+        //and fill in this output array with the data
+        String[] outputArray = new String[inputMatrix.size()];
 
-        //Applly the transforamtion and put the modified data into an ArrayList, not a matrix
-        String[] output = new String[content.size()];
         //The column names are fixed, so the first row is hard-coded.
         final String columnNames = "'nane'|'offerurl'|'price'|'published'|'description'";
-        output[0] = columnNames;
+        outputArray[0] = columnNames;
 
+        //Iterate each row of the matrix, except the first row (i.e. column names)
         ArrayList<String> row = new ArrayList<>();
-
-        //Iterate the rows of the matrix, except the first row (i.e. column names)
-        for (int i = 1; i < content.size(); i++) {
-            //temp contains the whole information of one single product
-            row = content.get(i);
+        for (int i = 1; i < inputMatrix.size(); i++) {
+            //From here, "row" will contain the whole information of one single product
+            row = inputMatrix.get(i);
             String product = "";
             //The current row will transform into one product
-            product = applyTransformation(product, row);
-            output[i] = product;
-            System.out.println("DONE " + i + " : " + output[i]);
+            product = applyTransformation(row);
+            //Now, "product" contains all the information of one single product.
+            outputArray[i] = product;
+            //The following line can be used to see the detail of the product
+            //System.out.println("Transformation is done " + i + " : " + outputArray[i]);
         }
 
-        String outputCSV ="C:\\Users\\takaha\\Desktop\\SmartAssistant\\test.csv";
-        
+        writeCSV(outputArray, outputFile);
     }
 
+   /**
+    * This will change a string of price and return it in the required format.
+    * @param Price which may contain non-meric values and commas
+    * @return Price which is either in "___.__" or "__" format
+    */
     public static String changePriceFormat(String price) {
-        //Erase non-numeric values other than "." and "," (e.g. USD or EUR)
+        //Remove non-numeric values other than "." and "," (e.g. USD or EUR)
         price = price.replaceAll("[^0-9.,]", "");
 
         //Then, replace "," with "."
@@ -87,8 +100,8 @@ public class SmartAssistant_Assignment {
 
         //Split the value by each dot
         String[] priceArray = price.split("\\.");
-        StringBuilder stringBuilder = new StringBuilder();
 
+        StringBuilder stringBuilder = new StringBuilder();
         //Remove all the unnecessary dots
         if (priceArray.length >= 2) {
             for (int x = 0; x < (priceArray.length - 1); x++) {
@@ -103,28 +116,41 @@ public class SmartAssistant_Assignment {
         return price;
     }
 
+    /**
+     * This will get a string of description and return an array of strings,
+     * which contains the date and its format information
+     * (The process to change the format is separated)
+     * @param Description from the input file
+     * @return An array which contains the date in day/month/year order and its format information
+     */
     public static String[] getDateAndFormat(String description) {
         String[] date = {"", ""};
         String DATE_REGEX1 = "([1-9]|0[1-9]|1[012])[-.](0[1-9]|[12][0-9]|3[01])[-.](19|20)\\d\\d";//mm/dd/yyyy
         String DATE_REGEX2 = "(0[1-9]|[12][0-9]|3[01])[-.]([1-9]|0[1-9]|1[012])[-.](19|20)\\d\\d";//dd/mm/yyyy
 
-        Matcher m1 = Pattern.compile(DATE_REGEX1).matcher(description);
-        Matcher m2 = Pattern.compile(DATE_REGEX2).matcher(description);
+        Matcher mm_dd_yyyy = Pattern.compile(DATE_REGEX1).matcher(description);
+        Matcher dd_mm_yyyy = Pattern.compile(DATE_REGEX2).matcher(description);
 
-        if (m1.find()) {
-            date[0] = m1.group();
+        if (mm_dd_yyyy.find()) {
+            date[0] = mm_dd_yyyy.group();
             date[1] = "mm/dd/yyyy";
-        } else if (m2.find()) {
-            date[0] = m2.group();
+        } else if (dd_mm_yyyy.find()) {
+            date[0] = dd_mm_yyyy.group();
             date[1] = "dd/mm/yyyy";
         }
 
         return date;
     }
 
+    /**
+     * This will change a date which in month/day/year format into
+     * dd.mm.yyyy format and return it 
+     * @param date in month/day/year order
+     * @return date in dd.mm.yyyy format
+     * @throws ParseException 
+     */
     public static String changeDateFormat(String date) throws ParseException {
         List<SimpleDateFormat> knownPatterns = new ArrayList<SimpleDateFormat>();
-        knownPatterns.add(new SimpleDateFormat("mm/dd/yyyy"));
         knownPatterns.add(new SimpleDateFormat("mm-dd-yyyy"));
         knownPatterns.add(new SimpleDateFormat("mm.dd.yyyy"));
         final String NEW_FORMAT = "dd.mm.yyyy";
@@ -143,10 +169,17 @@ public class SmartAssistant_Assignment {
         return date;
     }
 
+    /**
+     * This will revise a date which is in day/month/year order, since it sometimes has
+     * only one digit in its day. Also, sometimes a hyphen is used instead of a comma.
+     * If any of them is identified in the input date, it will be fixed (e.g., 1-12-2015 will be 01.12.2015)
+     * and then returned.
+     * @param Date in day/month/year order
+     * @return Date in dd.mm.yyyy format
+     */
     public static String reviseDateFormat(String date) {
         List<SimpleDateFormat> knownPatterns = new ArrayList<SimpleDateFormat>();
-        knownPatterns.add(new SimpleDateFormat("dd/mm/yyyy"));
-        knownPatterns.add(new SimpleDateFormat("dd-mm-yyyy"));
+        knownPatterns.add(new SimpleDateFormat("dd-mm-yyyy"));//This is not used for this particular task.
         knownPatterns.add(new SimpleDateFormat("dd.mm.yyyy"));
         final String NEW_FORMAT = "dd.mm.yyyy";
 
@@ -164,7 +197,17 @@ public class SmartAssistant_Assignment {
         return date;
     }
 
-    public static String applyTransformation(String product, ArrayList<String> row) throws ParseException {
+    /**
+     * This will apply the required transformation on the input, which is an array list
+     * containing information of one product from the input file.
+     * Then, the information will be returned in String.
+     * @param Row which contains information of one product from the input file
+     * @return String which contains information of one product with required transformation
+     * @throws ParseException 
+     */
+    public static String applyTransformation(ArrayList<String> row) throws ParseException {
+        String product = "";
+
         for (int l = 0; l < row.size(); l++) {
             switch (l) {
                 case 0://Name
@@ -182,7 +225,7 @@ public class SmartAssistant_Assignment {
                 case 4://description
                     String[] dateAndFormat = getDateAndFormat(row.get(l));
                     if (dateAndFormat[0] == "") {
-                        //This is when both "published" will be empty
+                        //This is when "description" is empty
                         product = product + "||'" + row.get(l) + "'";
                     } else if (dateAndFormat[1] == "mm/dd/yyyy") {
                         //The format needs to be changed from "mm/dd/yyyy" to "dd/mm/yyyy"
@@ -192,17 +235,26 @@ public class SmartAssistant_Assignment {
                         product = product + "|'" + reviseDateFormat(dateAndFormat[0]) + "'|'" + row.get(l) + "'";
                     }
                     break;
-                default:
-                    product = "invalid information was detected";
+                default://This would be invoked if there are 6 or more columns in the input CSV
+                    product = "The input data is invalid";
                     break;
             }
         }
         return product;
-
     }
 
-    public static void writeCSV(String[] output, String FileUrl){
-    
-        
+    /**
+     * This will write information all the products into the designated directory, where there is supposed
+     * to be a CSV file.
+     * All the information of the products are in an array of String.
+     * @param output
+     * @param FileUrl
+     * @throws IOException 
+     */
+    public static void writeCSV(String[] output, String FileUrl) throws IOException {
+        CSVWriter writer = new CSVWriter(new OutputStreamWriter(new FileOutputStream(FileUrl), "ISO-8859-1"), '\n');
+
+        writer.writeNext(output);
+        writer.close();
     }
 }
--- test/Test/SmartAssistant_Assignment_Test.java
+++ test/Test/SmartAssistant_Assignment_Test.java
@@ -6,48 +6,48 @@
 package Test;
 
 import java.text.ParseException;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import smartassistant_assignment.SmartAssistant_Assignment;
 
 /**
  *
- * @author takaha
+ * @author Hiroki Takahashi
  */
 public class SmartAssistant_Assignment_Test {
-
-    public SmartAssistant_Assignment_Test() {
-    }
 
     @Test
     public void TestGetDateAndFormat() {
         SmartAssistant_Assignment tester = new SmartAssistant_Assignment();
         String[] dummy = {"dummyDate", "dummyFormat"};
 
+        //Description with no date
         dummy = tester.getDateAndFormat("aaa");
         assertEquals(dummy[0], "");
         assertEquals(dummy[1], "");
         System.out.println("Date: " + dummy[0] + " Format: " + dummy[1]);
 
+        //Description with dd.mm.yyyy date
         dummy = tester.getDateAndFormat("Here a date: 20.09.2014");
         assertEquals(dummy[0], "20.09.2014");
         assertEquals(dummy[1], "dd/mm/yyyy");
         System.out.println("Date: " + dummy[0] + " Format: " + dummy[1]);
 
+        //Description with dd.m.yyyy date
         dummy = tester.getDateAndFormat("Another date: 31.4.1999");
         assertEquals(dummy[0], "31.4.1999");
         assertEquals(dummy[1], "dd/mm/yyyy");
         System.out.println("Date: " + dummy[0] + " Format: " + dummy[1]);
 
+        //Description with mm-dd-yyyy date
         dummy = tester.getDateAndFormat("American date: 12-23-2003");
         assertEquals(dummy[0], "12-23-2003");
         assertEquals(dummy[1], "mm/dd/yyyy");
         System.out.println("Date: " + dummy[0] + " Format: " + dummy[1]);
 
+        //Description with m/dd/yyyy date
         dummy = tester.getDateAndFormat("One more date: 1.10.2014");
         assertEquals(dummy[0], "1.10.2014");
         assertEquals(dummy[1], "mm/dd/yyyy");
@@ -60,38 +60,47 @@ public class SmartAssistant_Assignment_Test {
 
         String dummy = "";
 
+        //Remove the currency name and comma
         dummy = tester.changePriceFormat("USD 1,232.99");
         assertEquals(dummy, "1232.99");
         System.out.println(dummy);
 
+        //Remove the currency name
         dummy = tester.changePriceFormat("USD 8654.56");
         assertEquals(dummy, "8654.56");
         System.out.println(dummy);
 
-        dummy = tester.changePriceFormat("345,99 [euro symbole]");
+        //Remove the currency symbol and replace the comma with a dot
+        dummy = tester.changePriceFormat("345,99 €");
         assertEquals(dummy, "345.99");
         System.out.println(dummy);
 
+        //Remove the currency name and replace the comma with a dot
         dummy = tester.changePriceFormat("EUR 1255,59");
         assertEquals(dummy, "1255.59");
         System.out.println(dummy);
 
+        //Remove the currency name
         dummy = tester.changePriceFormat("$34.00");
         assertEquals(dummy, "34.00");
         System.out.println(dummy);
 
+        //Remove the currency name, remove the unnecessary dot, and replace the comma with a dot 
         dummy = tester.changePriceFormat("431.333,0 EUR");
         assertEquals(dummy, "431333.0");
         System.out.println(dummy);
 
+        //Replace the comma with a dot 
         dummy = tester.changePriceFormat("34,99");
         assertEquals(dummy, "34.99");
         System.out.println(dummy);
 
+        //No change
         dummy = tester.changePriceFormat("234.99");
         assertEquals(dummy, "234.99");
         System.out.println(dummy);
 
+        //No change
         dummy = tester.changePriceFormat("42");
         assertEquals(dummy, "42");
         System.out.println(dummy);
@@ -102,22 +111,58 @@ public class SmartAssistant_Assignment_Test {
         SmartAssistant_Assignment tester = new SmartAssistant_Assignment();
 
         String dummy = "";
-        
+
+        //No change
         dummy = tester.reviseDateFormat("20.09.2014");
         System.out.println(dummy);
         assertEquals(dummy, "20.09.2014");
 
+        //There needs to be one more digits in the month
         dummy = tester.reviseDateFormat("31.4.1999");
         System.out.println(dummy);
         assertEquals(dummy, "31.04.1999");
 
+        //Change the order and separators
         dummy = tester.changeDateFormat("12-23-2003");
         System.out.println(dummy);
         assertEquals(dummy, "23.12.2003");
 
+        //Change the order and add one more "0" in the month
         dummy = tester.changeDateFormat("1.10.2014");
         System.out.println(dummy);
         assertEquals(dummy, "10.01.2014");
 
+    }
+
+    @Test
+    public void TestTransformation() throws ParseException {
+        SmartAssistant_Assignment tester = new SmartAssistant_Assignment();
+        String dummy = "";
+        ArrayList<String> dummyRow = new ArrayList<>();
+
+        //All the information will be concatenated into one string
+        String[] dummyList1 = new String[]{"Product 1", "http://dummy.com", "sku", "1234.0", "description with no date"};
+        dummyRow.addAll(Arrays.asList(dummyList1));
+        dummy = tester.applyTransformation(dummyRow);
+        System.out.println(dummy);
+        assertEquals(dummy, "'Product 1'|'http://dummy.com?id=sku'|'1234.0'||'description with no date'");
+
+        dummyRow.clear();
+
+        //All the information will be concatenated into one string
+        String[] dummyList2 = new String[]{"Product 2", "http://dummy.com", "sku159", "$10", "'Here a date: 20.09.2014"};
+        dummyRow.addAll(Arrays.asList(dummyList2));
+        dummy = tester.applyTransformation(dummyRow);
+        System.out.println(dummy);
+        assertEquals(dummy, "'Product 2'|'http://dummy.com?id=sku159'|'10'|'20.09.2014'|''Here a date: 20.09.2014'");
+
+        dummyRow.clear();
+
+        //All the information will be concatenated into one string
+        String[] dummyList3 = new String[]{"Product 3", "http://dummy.com", "SKU", "123,456.78 EUR"};
+        dummyRow.addAll(Arrays.asList(dummyList3));
+        dummy = tester.applyTransformation(dummyRow);
+        System.out.println(dummy);
+        assertEquals(dummy, "'Product 3'|'http://dummy.com?id=SKU'|'123456.78'");
     }
 }
